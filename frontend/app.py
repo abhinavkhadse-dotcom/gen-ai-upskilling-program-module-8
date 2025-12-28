dd20fe34d31e8fe4efb773da12c35cdafef049c9
Abhinav Khadse <abhinav.khadse@iauro.com>
2025-12-28 21:55:30 +0530

    Mock or integrate backend inside Streamlit

--- frontend/app.py
+++ frontend/app.py
@@ -4,7 +4,10 @@ import requests
 st.title("FastAPI + Streamlit Demo")
 
 number = st.number_input("Enter a number:", value=2)
+# this is for external api calls
+# if st.button("Calculate Square"):
+#     response = requests.get(f"http://backend:8000/square/{number}")
+#     data = response.json()
+#     st.write(f"The square of {number} is {data['square']}")
 if st.button("Calculate Square"):
-    response = requests.get(f"http://backend:8000/square/{number}")
-    data = response.json()
-    st.write(f"The square of {number} is {data['square']}")
+    st.write(f"The square of {number} is {number**2}")
